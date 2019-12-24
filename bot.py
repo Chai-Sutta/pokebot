c7757785d73e7d168738bb7a53bb15e8ec6c44ee
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2019-12-24 16:40:12 +0530

    pokebot: misc cleanup - remove commented code - add /about command and drop /hello

--- bot.py
+++ bot.py
@@ -1,26 +1,27 @@
+import os
 from telegram.ext import Updater, CommandHandler
 import pokepy
 from modules import Type
 
 START_TEXT = 'Hey! Pokedex here. Type /help to get list of commands'
+ABOUT_TEXT = 'Bot made by Abhay Kshatriya (vegeto1806 on Telegram).\nThe source is available at https://github.com/kshatriya-abhay/pokebot'
+HELP_TEXT = """
+List of available commands:
+/help
+/about - About the bot
+/type - Get a Pokemon's type(s) and type weaknesses
+"""
 
-poke_client = pokepy.V2Client(cache='in_disk', cache_location='/home/abhayk/.cache')
-
-def hello(update, context):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
-
+poke_client = pokepy.V2Client(cache='in_disk', cache_location=(os.environ['HOME']+'/.cache'))
 
 def start(update, context):
     update.message.reply_text(text = START_TEXT)
 
+def about(update, context):
+    update.message.reply_text(text = ABOUT_TEXT)
+
 def get_help(update, context):
-    update.message.reply_text("""
-List of available commands:
-/hello
-/help
-/type - Get a Pokemon's type(s) and type weaknesses
-      """)
+    update.message.reply_text(HELP_TEXT)
 
 def get_type(update, context):
 	if len(context.args) == 1:
@@ -31,11 +32,11 @@ def get_type(update, context):
 		update.message.reply_text("Usage: /type Pikachu")
 
 
+TOKEN_STRING = open('API_TOKEN','r').read().replace('\n','')
+updater = Updater(TOKEN_STRING, use_context=True)
 
-updater = Updater(open('API_TOKEN','r').read().replace('\n',''), use_context=True)
-
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('about', about))
 updater.dispatcher.add_handler(CommandHandler('help', get_help))
 updater.dispatcher.add_handler(CommandHandler('type', get_type))
 
--- modules/Type.py
+++ modules/Type.py
@@ -15,33 +15,16 @@ def fetch_type(api,pkm):
 	dmgh = []
 	dmg2 = []
 	for t in typeList:
-		# print('Type : ' + t.name)
 		ans = ans + 'Type : ' + t.name.capitalize() + '\n'
 		s1list = []
 		s2list = []
 		s3list = []
-		# s1 = ''
 		for tn in t.damage_relations.no_damage_from:
-			# s1 = s1 + (tn.name+' ')
 			s1list.append(tn.name)
-		# if s1 == '':
-			# s1 = '-'
-		# print('No Damage from '+s1)
-		# s2 = ''
 		for tn in t.damage_relations.half_damage_from:
-			# s2 = s2 + (tn.name+' ')
 			s2list.append(tn.name)
-		# if s2 == '':
-			# s2 = '-'
-		# print('1/2 Damage from '+s2)
-		# s3 = ''
 		for tn in t.damage_relations.double_damage_from:
-			# s3 = s3 + (tn.name+' ')
 			s3list.append(tn.name)
-		# if s3 == '':
-			# s3 = '-'
-		# print('2x Damage from '+s3)
-		# print('')
 		dmg0.append(s1list)
 		dmgh.append(s2list)
 		dmg2.append(s3list)
@@ -66,34 +49,29 @@ def fetch_type(api,pkm):
 		for t in d0:
 			st = st + (t.capitalize()+', ')
 		st = st[:-2]
-		# print('No Damage from '+st)
 		ans = ans + 'No Damage from ' + st + '\n'
 	if d25:
 		st = ''
 		for t in d25:
 			st = st + (t.capitalize()+', ')
 		st = st[:-2]
-		# print('1/4th Damage from '+st)
 		ans = ans + '1/4th Damage from ' + st + '\n'
 	if d50:
 		st = ''
 		for t in d50:
 			st = st + (t.capitalize()+', ')
 		st = st[:-2]
-		# print('Half Damage from '+st)
 		ans = ans + 'Half Damage from ' + st + '\n'
 	if d200:
 		st = ''
 		for t in d200:
 			st = st + (t.capitalize()+', ')
 		st = st[:-2]
-		# print('Double Damage from '+st)
 		ans = ans + 'Double Damage from ' + st + '\n'
 	if d400:
 		st = ''
 		for t in d400:
 			st = st + (t.capitalize()+', ')
 		st = st[:-2]
-		# print('4x Damage from '+st)
 		ans = ans + '4x Damage from ' + st + '\n'
 	return ans
