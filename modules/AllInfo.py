700c04d0eb9fbc7166fa17138fedc90edc37cc28
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2020-04-11 23:28:04 +0530

    pokebot: add Starter and AllInfo modules

---
+++ modules/AllInfo.py
@@ -0,0 +1,28 @@
+import pokepy
+from . import Ability, Learnset, Type
+
+def get_all_info(api, query):
+	ans = ''
+
+	try:
+		pikasp = api.get_pokemon_species(query)
+	except:
+		response = "Invalid pokemon name."
+		return response
+
+	for n in pikasp.names:
+		if n.language.name == 'en':
+			pname = n.name
+	PRE_URL = 'https://bulbapedia.bulbagarden.net/wiki/'
+	MID = pname + '_%28Pokémon%29'
+	B_URL = PRE_URL+MID
+
+	ans = 'Pokémon Name: ['+pname+']('+B_URL+')' + '\n\n'
+
+	ans = ans + Ability.get_ability(api,query)
+
+	ans = ans + Type.fetch_type(api,query) + '\n'
+
+	ans = ans + 'Earliest Learnset: ' + Learnset.get_learnset(api,query)
+
+	return ans
---
+++ modules/Starter.py
@@ -0,0 +1,20 @@
+import pokepy
+
+def get_my_pokemon(api, user_id):
+	query = (user_id % 807) + 1
+	response = ''
+	try:
+		pikasp = api.get_pokemon_species(query)
+	except:
+		response = "What? This shouldn't happen..."
+		return response
+
+	PRE_URL = 'https://bulbapedia.bulbagarden.net/wiki/'
+	pname = ''
+	for n in pikasp.names:
+		if n.language.name == 'en':
+			pname = n.name
+	MID = pname + '_%28Pokémon%29'
+	B_URL = PRE_URL+MID
+	response = 'Your unique starter pokémon is: ['+pname+']('+B_URL+')'
+	return response
